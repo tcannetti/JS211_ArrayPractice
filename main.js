51393eed635e67aa13bc11f164091736b70cb6ff
tcannetti <tcannetti@gmail.com>
2020-10-13 18:03:33 -0500

    first few homework problems for Array of Cars

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Create an array called cars which consists of 4 different types of cars as String type.
+// The first car type should be Ford and includes Honda.
+let cars = ["Ford", "Honda", "Toyota", "Subaru"];
+
+console.log(cars.length);
+
+// Create another array called moreCars with 4 more different types of cars.
+// The last car type should be Honda.
+let moreCars = ["Chevrolet", "BMW", "Audi", "Honda"];
+
+//Use the concat method to combine the cars and moreCars arrays into another array called totalCars.
+let totalCars = cars.concat(moreCars);
+
+console.log(totalCars);
+
+// Use the indexOf method to console.log the index of Honda.
+
+
+// Use the lastIndexOf method to console.log the index of Ford.
